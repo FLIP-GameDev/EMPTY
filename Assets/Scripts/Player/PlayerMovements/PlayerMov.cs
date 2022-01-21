a96750db07c97ad8af7d5ac59632b7bbf0c9dd32
HP-pro4140p <77773996+Harshit-pro4140p@users.noreply.github.com>
2022-01-21 12:42:07 +0530

    Sprinting

--- Assets/Scripts/Player/PlayerMovements/PlayerMov.cs
+++ Assets/Scripts/Player/PlayerMovements/PlayerMov.cs
@@ -6,19 +6,29 @@ public class PlayerMov : MonoBehaviour
 {
     public CharacterController controller;
 
+    float StartingSpeed;
+
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
+    public float SprintcameraFOVTime = 0.001f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundmask;
 
+    public float normalFOV;
+
     Vector3 velocity;
     bool isGrounded;
-    // Update is called once per frame
+    void Start()
+    {
+        StartingSpeed = speed;
+        normalFOV = Camera.main.fieldOfView;
+    }
     void Update()
     {
+        //Movement
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundmask);
 
         if(isGrounded && velocity.y < 0)
@@ -41,5 +51,17 @@ public class PlayerMov : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        //Sprinting
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, normalFOV + 30, SprintcameraFOVTime);
+            speed = StartingSpeed * 2;
+        }
+        else
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, normalFOV, SprintcameraFOVTime);
+            speed = StartingSpeed;
+        }
     }
 }
